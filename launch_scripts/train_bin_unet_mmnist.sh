77d38a23e7378b5cba56836497d6a251e7f0012a
Andres Herrera <97.andres.herrera@gmail.com>
2024-06-09 18:24:31 -0300

    train bin mmnist

--- launch_scripts/train_bin_unet_mmnist.sh
+++ launch_scripts/train_bin_unet_mmnist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=qr_mmnist
+#SBATCH --job-name=bin_mmnist
 #SBATCH --ntasks=1
 #SABTCH --mem=1024
 #SBATCH --time=48:00:00
@@ -8,10 +8,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=97.andres.herrera@gmail.com
-#SBATCH --output=sbatch_output/qr_mmnist.out
+#SBATCH --output=sbatch_output/bin_mmnist.out
 
 source /etc/profile.d/modules.sh
 
 cd /clusteruy/home/andres.herrera/cloudiness_probabilistic_forecasting
-python src/moving_mnist.py --model_name=qr --epochs=20 --batch_size=32 --quantiles=[0.1,0.25,0.5,0.75,0.9] --num_filters=16 --save_experiment=True
+python src/moving_mnist.py --model_name=bin_classifier --epochs=20 --batch_size=32 --num_bins=5 --num_filters=16 --save_experiment=True
 
