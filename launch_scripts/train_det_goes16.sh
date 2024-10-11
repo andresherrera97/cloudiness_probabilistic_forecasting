5e402ea46cddc85e4b8e5ba2f46272e7bb4b7799
andresherrera97 <97.andres.herrera@gmail.com>
2024-10-10 23:23:46 -0300

    Refactor launch script to train model on GOES-16 dataset

--- launch_scripts/train_det_goes16.sh
+++ launch_scripts/train_det_goes16.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=bin_uru2020
+#SBATCH --job-name=det_salto
 #SBATCH --ntasks=1
 #SBATCH --mem=1024
-#SBATCH --time=72:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --partition=normal
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=97.andres.herrera@gmail.com
-#SBATCH --output=sbatch_output/bin_uru2020.out
+#SBATCH --output=sbatch_output/det_salto.out
 
 source /etc/profile.d/modules.sh
 
 cd /clusteruy/home/andres.herrera/cloudiness_probabilistic_forecasting
-python src/train_satellite.py \
-    --model_name=bin_classifier \
-    --time_horizon=6 \
-    --epochs=100 \
-    --batch_size=8 \
-    --num_bins=5 \
+python src/train.py \
+    --dataset=goes16 \
+    --model_name=det \
+    --epochs=20 \
+    --batch_size=6 \
     --num_filters=16 \
     --save_experiment=True \
     --optimizer=adam \
-    --train_metric=crps \
-    --val_metric=crps \
-    --checkpoint_folder=five_bins_16_filters_uru \
     --scheduler=plateau
