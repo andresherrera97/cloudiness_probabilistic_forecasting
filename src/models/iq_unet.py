fix: optimize gradient zeroing and scheduler step in IQUNetPipeline

src/models/iq_unet.py
@@ -310,6 +310,9 @@ class IQUNetPipeline(ProbabilisticUNet):
                 # data to cuda if possible
                 in_frames = in_frames.to(device=device, dtype=self.torch_dtype)
                 out_frames = out_frames.to(device=device, dtype=self.torch_dtype)
+                self.optimizer.zero_grad(
+                    set_to_none=True
+                )  # More efficient than zero_grad()
 
                 # forward
                 with torch.autocast(device_type=device_type, dtype=self.torch_dtype):  # Enable mixed precision
@@ -324,7 +327,8 @@ class IQUNetPipeline(ProbabilisticUNet):
                 scaler.scale(loss).backward()
                 scaler.step(self.optimizer)
                 scaler.update()
-                self.optimizer.zero_grad(set_to_none=True)  # More efficient than zero_grad()
+                if isinstance(self.scheduler, torch.optim.lr_scheduler.OneCycleLR):
+                    self.scheduler.step()
 
                 train_loss_in_epoch_list.append(loss.detach().item())
 
@@ -376,7 +380,9 @@ class IQUNetPipeline(ProbabilisticUNet):
 
             val_loss_in_epoch = quantile_loss_in_epoch
 
-            if self.scheduler is not None:
+            if self.scheduler is not None and not isinstance(
+                self.scheduler, torch.optim.lr_scheduler.OneCycleLR
+            ):
                 self.scheduler.step(val_loss_in_epoch)
 
             if run is not None: