42e3a52edb5a920c32feeb32309f13b318d3f1b7
andresherrera97 <97.andres.herrera@gmail.com>
2025-06-10 22:59:20 -0300

    refactor: remove unused solar angle functions and integrate image resizing with cv2 for improved performance

--- src/crop_cmi_dataset.py
+++ src/crop_cmi_dataset.py
@@ -1,65 +1,10 @@
 import os
+import cv2
 import fire
-import calendar
-import datetime
 import tqdm
 import numpy as np
 from pathlib import Path
-
-
-def daily_solar_angles(year: int, doy: int):
-    """For solar angle computations."""
-    days_year = (1 if calendar.isleap(year) else 0) + 365
-    gamma = 2 * np.pi * (doy - 1) / days_year
-    delta = (
-        0.006918
-        - 0.399912 * np.cos(gamma)
-        + 0.070257 * np.sin(gamma)
-        - 0.006758 * np.cos(2 * gamma)
-        + 0.000907 * np.sin(2 * gamma)
-        - 0.002697 * np.cos(3 * gamma)
-        + 0.00148 * np.sin(3 * gamma)
-    )
-    eot = (
-        60
-        * 3.8196667
-        * (
-            0.000075
-            + 0.001868 * np.cos(gamma)
-            - 0.032077 * np.sin(gamma)
-            - 0.014615 * np.cos(2 * gamma)
-            - 0.040849 * np.sin(2 * gamma)
-        )
-    )
-    return delta, eot
-
-
-def solar_angles(lat, lon, delta, eot, h, m, s, ret_mask=True):
-    """
-    Compute cos(zenith) given a lat/lon and a time of day (UTC).
-    """
-    lat_r = lat * np.pi / 180
-    h_sol = h + m / 60 + lon / 15 + eot / 60 + s / 3600
-    w_rad = np.pi * (h_sol / 12 - 1)
-    cos_zen = np.sin(lat_r) * np.sin(delta) + np.cos(lat_r) * np.cos(delta) * np.cos(
-        w_rad
-    )
-
-    mask = cos_zen > 0
-    if cos_zen.size > 1:
-        cos_zen[~mask] = 0
-    elif cos_zen.size == 1:
-        cos_zen = cos_zen * mask
-    return (cos_zen, mask) if ret_mask else cos_zen
-
-
-def get_cosangs(dtime: datetime.datetime, lats: np.ndarray, lons: np.ndarray):
-    """Main wrapper for solar angles for a given date/time."""
-    delta, eot = daily_solar_angles(dtime.year, dtime.timetuple().tm_yday)
-    cosz, mask = solar_angles(
-        lats, lons, delta, eot, dtime.hour, dtime.minute, dtime.second
-    )
-    return cosz, mask
+import matplotlib.pyplot as plt
 
 
 def find_nearest_pixel(REF_LAT, REF_LON, target_lat, target_lon):
@@ -128,92 +73,157 @@ def convert_coordinates_to_pixel(lat, lon, REF_LAT, REF_LON, size, verbose=True)
     return c_lats, c_lons, x, y
 
 
+def plot_centered_square(
+    lat: float, lon: float, size: int, c_lats: np.ndarray, c_lons: np.ndarray
+) -> None:
+    row = size // 2 - 1  # Center of the crop
+    column = size // 2 - 1  # Center of the crop
+    box_size = 2  # 2x2 box
+
+    lat_box_0 = c_lats[row : row + box_size, column : column + box_size]
+    lon_box_0 = c_lons[row : row + box_size, column : column + box_size]
+
+    corner_lons = [
+        lon_box_0[0, 0],
+        lon_box_0[0, -1],
+        lon_box_0[-1, -1],
+        lon_box_0[-1, 0],
+        lon_box_0[0, 0],
+    ]
+    corner_lats = [
+        lat_box_0[0, 0],
+        lat_box_0[0, -1],
+        lat_box_0[-1, -1],
+        lat_box_0[-1, 0],
+        lat_box_0[0, 0],
+    ]
+
+    plt.title("Center of the crop")
+    plt.xlabel("Longitude (deg)")
+    plt.ylabel("Latitude (deg)")
+    plt.plot(lon, lat, "ro", markersize=8, markeredgecolor="black", markeredgewidth=2)
+    plt.plot(corner_lons, corner_lats, "r-", linewidth=2)  # Red square line
+    plt.show()
+
+
 def main(
-    dataset_path: str = "datasets/salto1024_all/",
-    output_path: str = "datasets/LE_Crop_64x64/",
-    metadata_path: str = "datasets/ABI_L2_CMIP_M6C02_G16/FULL_DISK/",
+    dataset_path: str = "datasets/LE_Crop_128x128/",
+    output_path: str = "datasets/les/",
+    metadata_path: str = "datasets/LE_Crop_128x128/metadata/",
     lat: float = -31.2827,
     lon: float = -57.9181,
-    size: float = 128,
-    downsample: int = 2,
-    just_metadata: bool = True,
 ):
-
-    lat_path = os.path.join(metadata_path, "lat.npy")
-    lon_path = os.path.join(metadata_path, "lon.npy")
+    lat_path = os.path.join(metadata_path, "lats.npy")
+    lon_path = os.path.join(metadata_path, "lons.npy")
 
     print("Loading metadata...")
-    REF_LAT = np.load(lat_path)
-    lat_data = np.ma.masked_array(REF_LAT[0], mask=REF_LAT[1])
-    REF_LON = np.load(lon_path)
-    lon_data = np.ma.masked_array(REF_LON[0], mask=REF_LON[1])
-    del REF_LAT, REF_LON
+    lats = np.load(lat_path)
+    lons = np.load(lon_path)
 
-    # 2) Convert user lat/lon to pixel coords
-    print("Converting coordinates to pixel...")
-    c_lats, c_lons, x, y = convert_coordinates_to_pixel(
-        lat, lon, lat_data, lon_data, size, True
+    size = lats.shape[0]
+
+    # Path(os.path.join(output_path, "crop_64x64_HR", "metadata")).mkdir(
+    #     parents=True, exist_ok=True
+    # )
+    # Path(os.path.join(output_path, "crop_64x64_MR", "metadata")).mkdir(
+    #     parents=True, exist_ok=True
+    # )
+    # Path(os.path.join(output_path, "crop_64x64_HR", "CMI")).mkdir(
+    #     parents=True, exist_ok=True
+    # )
+    # Path(os.path.join(output_path, "crop_64x64_MR", "CMI")).mkdir(
+    #     parents=True, exist_ok=True
+    # )
+    Path(os.path.join(output_path, "crop_64x64_MR_bilinear", "metadata")).mkdir(
+        parents=True, exist_ok=True
     )
-    del lat_data, lon_data
+    Path(os.path.join(output_path, "crop_64x64_MR_bilinear", "CMI")).mkdir(
+        parents=True, exist_ok=True
+    )
 
-    Path(os.path.join(output_path, "metadata")).mkdir(parents=True, exist_ok=True)
+    lats_HR = lats[size // 4 : -size // 4, size // 4 : -size // 4]
+    lons_HR = lons[size // 4 : -size // 4, size // 4 : -size // 4]
+    lats_MR = lats[::2, ::2]
+    lons_MR = lons[::2, ::2]
+    lats_MR_bilinear = cv2.resize(
+        lats,
+        (lats.shape[1] // 2, lats.shape[0] // 2),
+        interpolation=cv2.INTER_LINEAR,
+    )
+    lons_MR_bilinear = cv2.resize(
+        lons,
+        (lons.shape[1] // 2, lons.shape[0] // 2),
+        interpolation=cv2.INTER_LINEAR,
+    )
 
-    print(c_lats.shape)
-    print(type(c_lats))
+    plot_centered_square(lat=lat, lon=lon, size=size, c_lats=lats, c_lons=lons)
+    plot_centered_square(
+        lat=lat, lon=lon, size=64, c_lats=lats_MR, c_lons=lons_MR
+    )
+    plot_centered_square(
+        lat=lat,
+        lon=lon,
+        size=64,
+        c_lats=lats_HR,
+        c_lons=lons_HR,
+    )
+    plot_centered_square(
+        lat=lat,
+        lon=lon,
+        size=64,
+        c_lats=lats_MR_bilinear,
+        c_lons=lons_MR_bilinear,
+    )
 
-    if downsample > 1:
-        print("Downsampling...")
-        c_lats = c_lats[::downsample, ::downsample]
-        c_lons = c_lons[::downsample, ::downsample]
-        print(c_lats.shape)
-        print(type(c_lats))
+    # np.save(os.path.join(output_path, "crop_64x64_HR", "metadata", "lats"), lats_HR)
+    # np.save(os.path.join(output_path, "crop_64x64_HR", "metadata", "lons"), lons_HR)
+    # np.save(os.path.join(output_path, "crop_64x64_MR", "metadata", "lats"), lats_MR)
+    # np.save(os.path.join(output_path, "crop_64x64_MR", "metadata", "lons"), lons_MR)
+    np.save(os.path.join(output_path, "crop_64x64_MR_bilinear", "metadata", "lons"), lons_MR_bilinear)
+    np.save(os.path.join(output_path, "crop_64x64_MR_bilinear", "metadata", "lats"), lats_MR_bilinear)
 
-    np.save(os.path.join(output_path, "metadata", "lats"), c_lats.data)
-    np.save(os.path.join(output_path, "metadata", "lons"), c_lons.data)
-
-    if just_metadata:
-        print("Just metadata, exiting...")
-        return
-
-    for day in tqdm.tqdm(sorted(os.listdir(dataset_path))):
-        os.mkdir(os.path.join(output_path, "CMI", day))
-        os.mkdir(os.path.join(output_path, "cosangs", day))
-
-        images = os.listdir(os.path.join(dataset_path, day))
-        for image in images:
-            if not image.endswith(".npy"):
+    for day in tqdm.tqdm(sorted(os.listdir(os.path.join(dataset_path, "CMI")))):
+        files_in_day = sorted(
+            os.listdir(os.path.join(dataset_path, "CMI", day))
+        )
+        # Path(os.path.join(output_path, "crop_64x64_HR", "CMI", day)).mkdir(
+        #     parents=True, exist_ok=True
+        # )
+        # Path(os.path.join(output_path, "crop_64x64_MR", "CMI", day)).mkdir(
+        #     parents=True, exist_ok=True
+        # )
+        Path(os.path.join(output_path, "crop_64x64_MR_bilinear", "CMI", day)).mkdir(
+            parents=True, exist_ok=True
+        )
+        for day_img in files_in_day:
+            if not day_img.endswith(".npy"):
                 continue
-            # image = YYYY_DOY_UTC_HHMMSS.npy
-            planetary_reflectance = np.load(
-                os.path.join(dataset_path, day, image)
-            )  # uint8
-            # crop 32 x 32 center
-            border_size = (1024 - 32) // 2
-            planetary_reflectance_crop = planetary_reflectance[
-                border_size:-border_size, border_size:-border_size
-            ]
-            # Transform from uint8 to float32
-            planetary_reflectance_crop = (
-                planetary_reflectance_crop.astype(np.float32) / 255.0
-            )
 
-            # Parse time from filename
-            y4 = image.split("_")[0]
-            dayy = image.split("_")[1]
-            hh = image.split("_")[-1][:2]
-            mm = image.split("_")[-1][2:4]
-            ss = image.split("_")[-1][4:6]
-            file_dt = datetime.datetime.strptime(
-                f"{dayy}/{y4} {hh}:{mm}:{ss}", "%j/%Y %H:%M:%S"
-            )
-            # 1) Compute solar angles
-            cosangs, _ = get_cosangs(file_dt, c_lats, c_lons)
-            # Calculate Cloud and Moisture Imagery values
-            cmi = planetary_reflectance_crop * cosangs.data
+            day_img_path = os.path.join(dataset_path, "CMI", day, day_img)
 
-            # save values in respective folder
-            np.save(os.path.join(output_path, "CMI", day, image), cmi)
-            np.save(os.path.join(output_path, "cosangs", day, image), cosangs.data)
+            # Load the image data
+            img_data = np.load(day_img_path)
+
+            # Save cropped images
+            # np.save(
+            #     os.path.join(output_path, "crop_64x64_HR", "CMI", day, day_img),
+            #     img_data[size // 4 : -size // 4, size // 4 : -size // 4],
+            # )
+            # np.save(
+            #     os.path.join(output_path, "crop_64x64_MR", "CMI", day, day_img),
+            #     img_data[::2, ::2],
+            # )
+            img_data = img_data.astype(np.float32)  # Ensure float32 for cv2.resize
+            img_bilinear = cv2.resize(
+                img_data,
+                (img_data.shape[1] // 2, img_data.shape[0] // 2),
+                interpolation=cv2.INTER_LINEAR,
+            )
+            img_bilinear = img_bilinear.astype(np.float16)  # Ensure float32 for saving
+            np.save(
+                os.path.join(output_path, "crop_64x64_MR_bilinear", "CMI", day, day_img),
+                img_bilinear,
+            )
 
 
 if __name__ == "__main__":
